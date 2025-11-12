0c3974446373883c82377f3533678fce745746da
Tom Moertel <tom@moertel.com>
2025-11-12 16:15:11 -0500

    Add emacs, ssh-askpass, and virt-manager

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# this installs packages from fedora repos
+dnf5 install -y emacs emacs-magit ksshaskpass openssh-askpass tmux virt-manager
 
 # Use a COPR Example:
 #
